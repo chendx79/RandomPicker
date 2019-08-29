719aa7ad8db9ea9ca9eae2bf26b9ac402b519182
chendx79 <chendx79@hotmail.com>
2019-08-29 16:08:06 +0800

    showed names

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	// "github.com/lxn/walk"
-	// . "github.com/lxn/walk/declarative"
-	// "strings"
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
 	"github.com/xuri/excelize"
 	"fmt"
 	"os"
+	"log"
 )
 
 func readNamesFromExcelFile() []string{
@@ -33,29 +33,62 @@ func readNamesFromExcelFile() []string{
 }
 
 func main() {
+	mw := &MyMainWindow{model: NewEnvModel()}
+
+	if _, err := (MainWindow{
+		AssignTo: &mw.MainWindow,
+		Title:    "随机选择工具",
+		MinSize:  Size{240, 320},
+		Size:     Size{300, 400},
+		Layout:   VBox{MarginsZero: true},
+		Children: []Widget{
+			HSplitter{
+				Children: []Widget{
+					ListBox{
+						AssignTo: &mw.lb,
+						Model:    mw.model,
+					},
+					TextEdit{
+						AssignTo: &mw.te,
+						ReadOnly: true,
+					},
+				},
+			},
+		},
+	}.Run()); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type MyMainWindow struct {
+	*walk.MainWindow
+	model *EnvModel
+	lb    *walk.ListBox
+	te    *walk.TextEdit
+}
+
+type EnvModel struct {
+	walk.ListModelBase
+	items []string
+}
+
+func NewEnvModel() *EnvModel {
 	var names []string
 	names = readNamesFromExcelFile()
-	fmt.Println(len(names))
-	
-	// var inTE, outTE *walk.TextEdit
 
-	// MainWindow{
-	// 	Title:   "SCREAMO",
-	// 	MinSize: Size{600, 400},
-	// 	Layout:  VBox{},
-	// 	Children: []Widget{
-	// 		HSplitter{
-	// 			Children: []Widget{
-	// 				TextEdit{AssignTo: &inTE},
-	// 				TextEdit{AssignTo: &outTE, ReadOnly: true},
-	// 			},
-	// 		},
-	// 		PushButton{
-	// 			Text: "SCREAM",
-	// 			OnClicked: func() {
-	// 				outTE.SetText(strings.ToUpper(inTE.Text()))
-	// 			},
-	// 		},
-	// 	},
-	// }.Run()
+	m := &EnvModel{items: make([]string, len(names))}
+
+	for i, e := range names {
+		m.items[i] = e
+	}
+
+	return m
+}
+
+func (m *EnvModel) ItemCount() int {
+	return len(m.items)
+}
+
+func (m *EnvModel) Value(index int) interface{} {
+	return m.items[index]
 }
